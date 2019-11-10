25c1dc17862c072e2a93795ade47cbf85e9808c3
Albert Tugushev <albert@tugushev.ru>
2019-11-10 11:25:59 +0700

    save

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='testpackage',
-      version='0.1',
+setup(name='atugushev-testpackage',
+      version='0.1.12',
       description='The test Package',
       url='http://github.com/atugushev/test-package',
-      install_requires=['azure-common', 'azure-mgmt-compute'],
+      install_requires=['six'],
       author='Albert Tugushev',
       author_email='albert@tugushev.ru',
       license='MIT',
